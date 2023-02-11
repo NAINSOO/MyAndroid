ad0de05941c23562c4cf17e45a8a996e9f56ea90
Nainsoo <i12force@naver.com>
2023-02-11 13:02:23 +0900

    Rotating display, UI state is saved and recovered

--- UnitConvertor/app/src/main/java/com/example/unitconvertor/MainActivity.kt
+++ UnitConvertor/app/src/main/java/com/example/unitconvertor/MainActivity.kt
@@ -4,62 +4,91 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.core.text.isDigitsOnly
 import androidx.core.widget.addTextChangedListener
 import com.example.unitconvertor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
-    var isCmtom = true
+    private lateinit var binding : ActivityMainBinding
+    private var isCmtom = true
+    private var inputText:String = ""
+    private var outputText:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if(savedInstanceState != null){
+            inputText = savedInstanceState.getString("inputText").toString()
+            outputText = savedInstanceState.getString("outputText").toString()
+            isCmtom = savedInstanceState.getBoolean("isCmtom")
+            Log.d("sdsfdsdf","$inputText $outputText $isCmtom")
+            if(isCmtom) {
+                binding.unit1.text = "cm"
+                binding.unit2.text = "m"
+            }else{
+                binding.unit1.text = "m"
+                binding.unit2.text = "cm"
+            }
+
+        }
+
+
         binding.imgButton.setOnClickListener{
-            val str : String = binding.edit.text.toString()
-            if(isCmtom){
+            if(isCmtom) {
                 isCmtom = false
                 binding.unit1.text = "m"
                 binding.unit2.text = "cm"
-                binding.text.text = (str.toInt() * 100).toString()
             }else{
                 isCmtom = true
                 binding.unit1.text = "cm"
                 binding.unit2.text = "m"
-                binding.text.text = (str.toFloat() / 100).toString()
+            }
+
+            if(inputText.isEmpty()){
+                binding.text.text = "0"
+            }else if(!inputText.isDigitsOnly()){
+                binding.text.text = "잘못된 값"
+            }else {
+                outputText = if(isCmtom){
+                    (inputText.toFloat() / 100).toString()
+                }else{
+                    (inputText.toInt() * 100).toString()
+                }
+                binding.text.text = outputText
             }
         }
+        
+        binding.edit.addTextChangedListener {
+            inputText = it.toString()
 
-        binding.edit.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if(inputText.isEmpty()){ // EditText가 비었을 때 text값이 null이 아니다.
+                binding.text.text = "0"
+                return@addTextChangedListener
+            }else if(!inputText.isDigitsOnly()){
+                binding.text.text = "잘못된 값"
+                return@addTextChangedListener
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                val str : String? = binding.edit.text?.toString()
-
-                if(str == null){
-                    binding.edit.setText("0")
-                    binding.text.text = "0"
-                    return
-                }else if(!str.isDigitsOnly()){
-                    binding.edit.setText("0")
-                    return
-                }
-
-                if(isCmtom){
-                    binding.text.text = (str.toFloat() / 100).toString()
-                }else{
-                    binding.text.text = (str.toInt() * 100).toString()
-                }
-
+            outputText = if(isCmtom){
+                (inputText.toFloat() / 100).toString()
+            }else{
+                (inputText.toInt() * 100).toString()
             }
 
-            override fun afterTextChanged(p0: Editable?) {
-            }
+            binding.text.text = outputText
+        }
 
-        })
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("inputText", inputText)
+        outState.putString("outputText", outputText)
+        outState.putBoolean("isCmtom", isCmtom)
     }
 }
